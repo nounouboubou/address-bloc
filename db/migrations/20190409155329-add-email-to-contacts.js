f7d018dd0354ce41c588918fdb54650345d39425
Nour Bouzaffara <nbouzaffara@manh.com>
2019-04-11 11:30:27 +0200

    second attempt to create contacts

---
+++ db/migrations/20190409155329-add-email-to-contacts.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Contacts','email',{
+		Sequelize.STRING
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Contacts','email');
+  }
+};
