e07b900590f8af6ec7479b9d73ae60278c1fa402
Nour Bouzaffara <nbouzaffara@manh.com>
2019-04-13 16:41:34 +0200

    listing contacts

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -10,8 +10,9 @@
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+		  "View all contacts",
+		  "Search for a contact",
 		  "Get date and time",
-		  "Remind Me!",
           "Exit"
         ]
       }
@@ -29,6 +30,12 @@
            break;
 		 case "Get date and time":
 			this.getDate();
+			break;
+		 case "View all contacts":
+			this.getContacts();
+			break;
+		case "Search for a contact":
+			this.search();
 			break;
          case "Exit":
            this.exit();
@@ -52,7 +59,7 @@
    addContact(){
      this.clear();
      inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-       this.book.addContact(answers.name, answers.phone).then((contact) => {
+       this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
          console.log("Contact added successfully!");
          this.main();
        }).catch((err) => {
@@ -61,15 +68,40 @@
        });
      });
    }
+   
+    getContacts(){
+      this.clear();
 
+      this.book.getContacts().then((contacts) => {
+        for (let contact of contacts) {
+          console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+          );
+        }
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+	
+	
    exit(){
      console.log("Thanks for using AddressBloc!");
      process.exit();
    }
    
+   
+   
    getContactCount(){
 		return this.contacts.length;
    }
+   
+   
    
    getDate()
    {
@@ -118,10 +150,78 @@
 
    }
    
-   remindMe()
-   {
-	   return "Learning is a life-long poursuit";
-   }
+   
+   search(){
+      inquirer.prompt(this.book.searchQuestions)
+      .then((target) => {
+       this.book.search(target.name)
+       .then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+         }
+
+        });
+     })
+     .catch((err) => {
+       console.log(err);
+       this.main();
+     });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+	  inquirer.prompt(this.book.showContactQuestions)
+	  .then((answer) => {
+		switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+     .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+    }
+
+    _printContact(contact);
+      console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+      );
+    }
+	
+	
+	delete(contact){
+      inquirer.prompt(this.book.deleteConfirmQuestions)
+      .then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+   
  }
  
  
